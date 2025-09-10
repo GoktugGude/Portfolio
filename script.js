609904bee5293985551dbe7b420d8a6879b61dbc
Goktug Gude <gudegoktug@hotmail.com>
2025-09-10 12:53:10 +0300

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const titles = document.querySelector(".titles");
+
+hamburger.addEventListener("click", () =>{
+        hamburger.classList.toggle("active");
+})
